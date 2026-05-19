35a3504f8a857d88d930cc3b2a142981ec682485
Itamar Nascimento <itamarasa@gmail.com>
2026-05-19 09:13:15 -0300

    feat: initialize axios instance with request interceptor for token injection and cache busting

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.10.14:5000/api",
+  baseURL: process.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
